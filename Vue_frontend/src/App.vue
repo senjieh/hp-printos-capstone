2530fb7eec4786de8fed788845ea89503ac9c334
senjieh <ssenjieh@gmail.com>
2023-10-25 20:04:59 -0700

    installed primevue component library

--- Vue_frontend/src/App.vue
+++ Vue_frontend/src/App.vue
@@ -1,29 +1,55 @@
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const text = ref('');
+    const dates = ref([]);  // Declare dates as an array
+    return { text, dates }; // Return dates so it's available in the template
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ['January', 'February', 'March', 'April', 'May'],
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <ApiPractice />
+  <div class="h-container">
+    <span class="p-float-label">
+      <InputText id="txt" type="text" v-model="text"/>
+      <label for="txt">Printer ID</label>
+    </span>
+
+
+    <PrimeCalender v-model="dates" selectionMode="multiple" :manualInput="false" />
+    <PrimeChart type="line" :data="chartData"></PrimeChart>
+    <PrimeButton label="doesnothing">Get Data</PrimeButton>
   </div>
 </template>
 
-<script>
-//import HelloWorld from './components/HelloWorld.vue'
-import ApiPractice from './components/ApiPractice.vue'
-
-export default {
-  name: 'App',
-  components: { //list of components
-    ApiPractice
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.v-container{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+.h-container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
