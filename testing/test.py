fdb18b45b5c41bcc38f8fc1c76f01c1b14b1767c
Nanikoreee <jacobhua1@gmail.com>
2023-11-08 19:09:42 -0800

    Removed always fail case in test.py

--- testing/test.py
+++ testing/test.py
@@ -3,6 +3,4 @@ from unittest import TestCase
 class Test(TestCase):
     def testAlwaysPasse(self):
         self.assertTrue(True)
-
-    def testAlwaysFails(self):
-        self.assertTrue(False)
+        
