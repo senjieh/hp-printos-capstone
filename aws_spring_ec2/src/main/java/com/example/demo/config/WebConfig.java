2e2daf6f545aa0a9f8cdaf4a05b4639284497d21
senjieh <ssenjieh@gmail.com>
2024-03-27 14:12:34 -0700

    Update WebConfig.java

--- aws_spring_ec2/src/main/java/com/example/demo/config/WebConfig.java
+++ aws_spring_ec2/src/main/java/com/example/demo/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://ec2-3-145-70-195.us-east-2.compute.amazonaws.com", "http://localhost:8080",
-                        "http://localhost:8081") // adjust the port
+                        "http://localhost:8081", "https://hp-printos-capstone-gilt.vercel.app/") // adjust the port
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
